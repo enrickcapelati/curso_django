397a226d26b52bfbd6cb1c80a431cb1a4dd58cac
Enrick Capelati <enrickcapelati@hotmail.com>
2022-02-10 22:22:18 -0300

    Organização e Css

--- meu_site/blog/models.py
+++ meu_site/blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset()\
+                                           .filter(status='publicado')
 
 class Post (models.Model):
     STATUS  = (
@@ -19,6 +23,9 @@ class Post (models.Model):
     status     = models.CharField(max_length=10,
                                   choices=STATUS,
                                   default='rascunho')
+
+    objects   = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publicado',)
