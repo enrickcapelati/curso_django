8c0fdf64b11ee1b7ab3f73444d31f466fbdcb326
Enrick Capelati <enrickcapelati@hotmail.com>
2022-02-10 23:25:40 -0300

    Criação do DetailView

--- meu_site/blog/urls.py
+++ meu_site/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
+    path('post/<slug:slug>/', views.BlogDetailView.as_view(), name='post_detail'),
+
 ]
+
--- meu_site/blog/views.py
+++ meu_site/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import Post
+from django.views.generic import ListView, DetailView
+from . models import Post
 
 class BlogListView (ListView):
-    model = Post
+    model         = Post
     template_name = 'blog/home.html'
+
+
+class BlogDetailView (DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'custom'
+
+
