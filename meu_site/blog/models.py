6dff82319920b8e9eea7876dd1f295bb2f9115be
Enrick Capelati <enrickcapelati@hotmail.com>
2022-02-10 22:36:27 -0300

    Organização e Css2

--- meu_site/blog/models.py
+++ meu_site/blog/models.py
@@ -7,22 +7,22 @@ class PublishedManager(models.Manager):
         return super(PublishedManager,self).get_queryset()\
                                            .filter(status='publicado')
 
-class Post (models.Model):
-    STATUS  = (
-        ("rascunho","Rascunho"),
-        ("publicação", "Publicação")
+class Post(models.Model):
+    STATUS = (
+        ('rascunho','Rascunho'),
+        ('publicado','Publicado'),
     )
-    titulo  = models.CharField  (max_length=250)
-    slug    = models.SlugField  (max_length=250)
-    autor   = models.ForeignKey (User,
-                                 on_delete=models.CASCADE)
-    conteudo   = models.TextField()
-    publicado  = models.DateTimeField(default=timezone.now)
-    criado     = models.DateTimeField(auto_now_add=True)
-    alterado   = models.DateTimeField(auto_now=True)
-    status     = models.CharField(max_length=10,
-                                  choices=STATUS,
-                                  default='rascunho')
+    titulo = models.CharField(max_length=250)
+    slug   = models.SlugField(max_length=250)#https://site.com/noticias/campeonato-brasileiro-2020/01/02/2020
+    autor  = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    conteudo  = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now)
+    criado    = models.DateTimeField(auto_now_add=True)
+    alterado  = models.DateTimeField(auto_now=True)
+    status    = models.CharField(max_length=10,
+                                 choices=STATUS,
+                                 default='rascunho')
 
     objects   = models.Manager()
     published = PublishedManager()
@@ -30,7 +30,5 @@ class Post (models.Model):
     class Meta:
         ordering = ('-publicado',)
 
-
     def __str__(self):
         return self.titulo
-
