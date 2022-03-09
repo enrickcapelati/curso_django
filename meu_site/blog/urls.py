f15d0c1aac6e1be10ca3dc2c541db04b2dabe974
Enrick Capelati <enrickcapelati@hotmail.com>
2022-03-09 14:57:29 -0300

    Delete Post

--- meu_site/blog/urls.py
+++ meu_site/blog/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/<slug:pk>/', views.BlogDetailView.as_view(),name='post_detail'),
     path('', views.BlogListView.as_view(),name='home'),
     path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(),name='post_detail'),
     path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(),name='post_edit'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
 
 ]
